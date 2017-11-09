d4268ba0cf66137eb4800f408b2efef1970593b2
ZHDKk <1083861095@qq.com>
2017-11-09 09:12:46 +0800

    第一次提交

--- JSON文件格式/ViewController.m
+++ JSON文件格式/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+//声明数组字典
+static NSDictionary *dic;
 @interface ViewController ()
 
 @end
@@ -17,6 +19,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //获取json文件在手机中的路径    参数一：文件的名字。  参数二：文件扩展名
+    NSString *path = [[NSBundle mainBundle]pathForResource:@"" ofType:@"json"];
+    //将文件读取出来，做为二进制存储在内存中
+    NSData *dataJson = [NSData dataWithContentsOfFile:path];
+    
+    //将数据解析类。  参数一：二进制数据对象。  参数二：解析方式，默认解析方式解析   参数三：错误信息对象，可以传nil
+    dic = [NSJSONSerialization JSONObjectWithData:dataJson options:NSJSONReadingMutableContainers error:nil];
+}
+
+-(void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"dataJson = %@",dic);
 }
 
 
